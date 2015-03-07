821bf4fdbfbdb3a8101034a541b6b85eb7ac61fc
777jon <jcruzdeveloper@gmail.com>
2015-03-07 07:23:26 -0500

    d852: Euphorize
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- eos.mk
+++ eos.mk
@@ -18,11 +18,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from d852 device
-$(call inherit-product, device/lge/d852/device.mk)
+$(call inherit-product, device/lge/d852/d852.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d852
-PRODUCT_NAME := full_d852
+PRODUCT_NAME := eos_d852
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D852
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_bell_ca" \
+    BUILD_FINGERPRINT="lge/g3_bell_ca/g3:4.4.2/KVT49L.D85210b/D85210b.1405397224:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_bell_ca-user 4.4.2 KVT49L.D85210b D85210b.1405397224 release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
